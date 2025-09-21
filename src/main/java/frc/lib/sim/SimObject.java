f6e9b2946e2092ff3a661cd236accf64a4e36502
Daniel Quednow <danquednow@gmail.com>
2025-09-21 16:58:09 -0400

    Add unit documentation comments

--- src/main/java/frc/lib/sim/SimObject.java
+++ src/main/java/frc/lib/sim/SimObject.java
@@ -5,8 +5,14 @@ import edu.wpi.first.wpilibj.Timer;
 public abstract class SimObject {
     private double lastFPGASeconds;
 
+    /**
+     * Gets the position of the mechanism in it's units (meters for linear, radians for angular)
+     */
     public abstract double getPosition();
 
+    /**
+     * Gets the velocity of the mechanism in its units (m/s for linear, rad/s for angular)
+     */
     public abstract double getVelocity();
 
     public abstract double getStatorCurrent();
