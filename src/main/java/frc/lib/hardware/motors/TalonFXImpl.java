ea63c9ad161ff6f18c13325ea4c176b6c4362839
Vimal Buckley <vimalbuckley@gmail.com>
2025-09-21 10:57:07 -0400

    Implemented MotorIO methods

--- src/main/java/frc/lib/hardware/motors/TalonFXImpl.java
+++ src/main/java/frc/lib/hardware/motors/TalonFXImpl.java
@@ -1,6 +1,9 @@
 package frc.lib.hardware.motors;
 
 import com.ctre.phoenix6.CANBus;
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
+import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 import frc.lib.io.motor.MotorIO;
@@ -8,6 +11,9 @@ import frc.lib.io.motor.MotorOutputs;
 
 public class TalonFXImpl extends MotorIO {
     private TalonFX[] motors;
+    private PositionVoltage positionRequest;
+    private VelocityVoltage velocityRequest;
+    private MotionMagicVoltage motionMagicRequest;
 
     public TalonFXImpl(CANBus canbus, int... ids) {
         super(ids.length);
@@ -19,49 +25,50 @@ public class TalonFXImpl extends MotorIO {
 
     @Override
     protected void updateOutputs(MotorOutputs[] outputs) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'updateOutputs'");
+        for (int i = 0; i <outputs.length; i++) {
+            outputs[i] = new MotorOutputs(
+                motors[i].getPosition().getValueAsDouble(), 
+                motors[i].getVelocity().getValueAsDouble(), 
+                motors[i].getMotorVoltage().getValueAsDouble(), 
+                motors[i].getStatorCurrent().getValueAsDouble(), 
+                motors[i].getSupplyCurrent().getValueAsDouble(), 
+                motors[i].getDeviceTemp().getValueAsDouble()
+            );
+        }
     }
 
     @Override
     protected void setVoltage(double voltage) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setVoltage'");
+        motors[0].setVoltage(voltage);
     }
 
     @Override
     protected void setCurrent(double current) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setCurrent'");
+        throw new UnsupportedOperationException("TalonFX does not support current control without Phoenix Pro");
     }
 
     @Override
     protected void setPosition(double position) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setPosition'");
+        motors[0].setControl(positionRequest.withPosition(position));
     }
 
     @Override
     protected void setVelocity(double velocity) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setVelocity'");
+        motors[0].setControl(velocityRequest.withVelocity(velocity));
     }
 
     @Override
     protected void setProfiledPosition(double position) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setProfiledPosition'");
+        motors[0].setControl(motionMagicRequest.withPosition(position));
     }
 
     @Override
     protected void setPercentage(double percentage) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setPercentage'");
+        motors[0].set(percentage);
     }
 
     @Override
     protected void setIdle() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setIdle'");
+        motors[0].setVoltage(0);
     }
 }
