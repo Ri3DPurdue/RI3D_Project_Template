220834717c93d4312531a231fb4ac1870579dbb1
Daniel Quednow <danquednow@gmail.com>
2025-09-21 14:33:31 -0400

    Update motor outputs
    Added supply voltage to the outputs too
    I'm starting to get unsure about how I feel about this because it is
    starting to feel like a lot, especially if it is for every motor, but
    whatever

--- src/main/java/frc/lib/io/motor/MotorOutputs.java
+++ src/main/java/frc/lib/io/motor/MotorOutputs.java
@@ -3,26 +3,31 @@ package frc.lib.io.motor;
 public class MotorOutputs {
     public double position;
     public double velocity;
-    public double voltage;
+
+    public double supplyVoltage;
+    public double statorVoltage;
+    
     public double statorCurrent;
     public double supplyCurrent;
     public double temperatureCelsius;
 
     public MotorOutputs() {
-        this(0, 0, 0, 0, 0, 0);
+        this(0, 0, 0, 0, 0, 0, 0);
     }
 
     public MotorOutputs(
         double position,
         double velocity,
-        double voltage,
+        double supplyVoltage,
+        double statorVoltage,
         double statorCurrent,
         double supplyCurrent,
         double temperatureCelsius
     ) {
         this.position = position;
         this.velocity = velocity;
-        this.voltage = voltage;
+        this.supplyVoltage = supplyVoltage;
+        this.statorVoltage = statorVoltage;
         this.statorCurrent = statorCurrent;
         this.supplyCurrent = supplyCurrent;
         this.temperatureCelsius = temperatureCelsius;
