6b76ecb76b562c3f2c2d52dc87fe24fa26d89b3d
Daniel Quednow <danquednow@gmail.com>
2025-09-21 12:55:15 -0400

    Update MotorOutputs
    I didn't realize that records are stupid and immutable, so this fixes
    that issue

--- src/main/java/frc/lib/io/motor/MotorOutputs.java
+++ src/main/java/frc/lib/io/motor/MotorOutputs.java
@@ -1,14 +1,30 @@
 package frc.lib.io.motor;
 
-public record MotorOutputs(
-    double position,
-    double velocity,
-    double voltage,
-    double statorCurrent,
-    double supplyCurrent,
-    double temperatureCelsius
-) {
+public class MotorOutputs {
+    public double position;
+    public double velocity;
+    public double voltage;
+    public double statorCurrent;
+    public double supplyCurrent;
+    public double temperatureCelsius;
+
     public MotorOutputs() {
         this(0, 0, 0, 0, 0, 0);
+    }
+
+    public MotorOutputs(
+        double position,
+        double velocity,
+        double voltage,
+        double statorCurrent,
+        double supplyCurrent,
+        double temperatureCelsius
+    ) {
+        this.position = position;
+        this.velocity = velocity;
+        this.voltage = voltage;
+        this.statorCurrent = statorCurrent;
+        this.supplyCurrent = supplyCurrent;
+        this.temperatureCelsius = temperatureCelsius;
     }
 }
