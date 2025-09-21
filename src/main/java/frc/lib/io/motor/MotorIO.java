2ca4a0b63a68aefd5f5c7322d78b31a6772085d1
Daniel Quednow <danquednow@gmail.com>
2025-09-20 20:32:44 -0400

    Add documentation comments
    Added documentation for the non obvious MotorIO methods, especially when
    they have assumptions baked in
    Also made sure to initialize the array of motor outputs

--- src/main/java/frc/lib/io/motor/MotorIO.java
+++ src/main/java/frc/lib/io/motor/MotorIO.java
@@ -2,8 +2,6 @@ package frc.lib.io.motor;
 
 import static frc.lib.io.motor.Setpoint.Type;
 
-import edu.wpi.first.math.system.plant.DCMotor;
-
 public abstract class MotorIO {
     private Setpoint currentSetpoint;
     private boolean enabled;
@@ -12,19 +10,44 @@ public abstract class MotorIO {
     protected MotorIO(int numFollowers) {
         currentSetpoint = new Setpoint(Type.Idle, 0);
         outputs = new MotorOutputs[numFollowers + 1];
+        for (int i = 0; i < numFollowers + 1; i++) {
+            outputs[i] = new MotorOutputs();
+        }
+        
         enabled = true;
     }
 
+    /**
+     * Enables the motor, causing it to take the previously set setpoint
+     * Note: A setpoint will still be applied, even if the motor is disabled,
+     * it just won't take effect until the motor is re-enabled
+     */
     public final void enable() {
         enabled = true;
         applySetpoint(currentSetpoint);
     }
 
+    /**
+     * Disabled the motor, causing it to idle until re-enabled
+     * Note: Setpoints will still be applied, but it will only take effect once
+     * the motor gets re-enabled
+     */
     public final void disable() {
         enabled = false;
         setIdle();
     }
 
+    public final boolean isEnabled() {
+        return enabled;
+    }
+
+    /**
+     * Applies the given setpoint to the motor
+     * Note: Copies the value from the supplied setpoint so you can keep
+     * ownership of the value supplied
+     * 
+     * @param setpoint
+     */
     public final void applySetpoint(Setpoint setpoint) {
         if (enabled) {
             currentSetpoint.set(setpoint);
@@ -61,18 +84,35 @@ public abstract class MotorIO {
         }
     }
 
+    /**
+     * Gets the current setpoint
+     * @implNote This does get updated even when the motor is disabled
+     * @implNote This returns a copy of the current setpoint, so feel free to modify the data
+     */
     public final Setpoint getCurrentSetpoint() {
         return currentSetpoint.clone();
     }
 
+    /**
+     * Gets the outputs from all the motors
+     * @implNote This returns the internal array, so be careful about modifying it
+     */
     public final MotorOutputs[] getOutputs() {
         return outputs;
     }
 
+    /**
+     * Method to be run periodically for a motor
+     * @implNote This is intended to be able to be overridden by subclasses
+     */
     public void periodic() {
         updateOutputs(outputs);
     }
 
+    /**
+     * Gets the outputs for the motor
+     * @param outputs The array to write the outputs into
+     */
     protected abstract void updateOutputs(MotorOutputs[] outputs);
 
     protected abstract void setVoltage(double voltage);
@@ -84,6 +124,4 @@ public abstract class MotorIO {
 
     protected abstract void setPercentage(double percentage);
     protected abstract void setIdle();
-
-    public abstract DCMotor getMotor();
 }
--- src/main/java/frc/lib/io/motor/Setpoint.java
+++ src/main/java/frc/lib/io/motor/Setpoint.java
@@ -1,5 +1,10 @@
 package frc.lib.io.motor;
 
+/**
+ * Class that defines a setpoint to a MotorIO
+ * Holds what kind of setpoint it is (Motor output type) and what value it takes
+ * The interpretation of the value depends on the setpoint type
+ */
 public class Setpoint {
     public static enum Type {
         Voltage,
@@ -31,11 +36,17 @@ public class Setpoint {
         this.value = value;
     }
 
+    /**
+     * Copies the value from the given setpoint
+     */
     public void set(Setpoint other) {
         this.outputType = other.outputType;
         this.value = other.value;
     }
 
+    /**
+     * Creates a new setpoint with the same values as the current one
+     */
     public Setpoint clone() {
         Setpoint newSetpoint = new Setpoint();
         newSetpoint.set(this);
