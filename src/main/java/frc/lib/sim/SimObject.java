dfeb8b9d201e4ada7b3b5def4017c4ce66142a66
Joshua Barrett <jb74911@djusdstudents.org>
2025-09-21 13:09:58 -0400

    add basic sim object abstract class

---
+++ src/main/java/frc/lib/sim/SimObject.java
@@ -0,0 +1,27 @@
+package frc.lib.sim;
+
+import edu.wpi.first.wpilibj.Timer;
+
+public abstract class SimObject {
+    private double lastFPGASeconds;
+
+    public abstract double getPosition();
+
+    public abstract double getVelocity();
+
+    public abstract double getStatorCurrent();
+
+    public abstract double setVoltage(double volts);
+
+    protected abstract void simulate(double deltaSeconds);
+
+    public void update() {
+        double currentFPGASeconds = Timer.getFPGATimestamp();
+        simulate(currentFPGASeconds - lastFPGASeconds);
+        lastFPGASeconds = currentFPGASeconds;
+    }
+
+    protected SimObject() {
+        lastFPGASeconds = Timer.getFPGATimestamp();
+    }
+}
