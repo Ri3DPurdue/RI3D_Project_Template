c44611174f745af74ec70fd6634ff23813963b10
Daniel Quednow <danquednow@gmail.com>
2025-10-12 15:19:47 -0400

    Make DigitalIO loggable
    We are going to be using it a lot, in theory so it would probably be a
    good idea to make it loggable and not have to write the same code many
    times over

--- src/main/java/frc/lib/io/sensor/DigitalIO.java
+++ src/main/java/frc/lib/io/sensor/DigitalIO.java
@@ -1,26 +1,43 @@
 package frc.lib.io.sensor;
 
+import org.littletonrobotics.junction.AutoLog;
+import org.littletonrobotics.junction.Logger;
+
 import edu.wpi.first.math.filter.Debouncer;
 import edu.wpi.first.math.filter.Debouncer.DebounceType;
+import frc.lib.io.logging.Loggable;
 
-public abstract class DigitalIO {
+public abstract class DigitalIO implements Loggable {
+    @AutoLog
+    public static class DigitalIOOutputs {
+        public boolean raw;
+        public boolean debounced;
+    }
+
     private final Debouncer debouncer;
-    private boolean lastRawValue;
-    private boolean lastDebouncedValue;
+    private final DigitalIOOutputsAutoLogged outputs;
 
     public DigitalIO(double debounceSeconds) {
 		debouncer = new Debouncer(debounceSeconds, DebounceType.kBoth);
+        outputs = new DigitalIOOutputsAutoLogged();
         update();
 	}
 
 	public abstract boolean get();
 
     public void update() {
-        lastRawValue = get();
-        lastDebouncedValue = debouncer.calculate(lastRawValue);
+        outputs.raw = get();
+        outputs.debounced = debouncer.calculate(outputs.raw);
     }
 
 	public boolean getDebounced() {
-		return lastDebouncedValue;
+		return outputs.debounced;
 	}
+
+    @Override
+    public void log(String subdirectory, String name) {
+        String path = subdirectory + "/" + name;
+
+        Logger.processInputs(path, outputs);
+    }
 }
