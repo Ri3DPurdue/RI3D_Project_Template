5137c05e6bbd335ea459775ad73ac9432fe2a120
Daniel Quednow <danquednow@gmail.com>
2025-10-16 08:46:31 -0400

    Make profiled position a position setpoint
    Basically, it is just easier to not have to write the code for both, so
    polymorphism baby!

--- src/main/java/frc/lib/io/motor/setpoints/ProfiledPositionSetpoint.java
+++ src/main/java/frc/lib/io/motor/setpoints/ProfiledPositionSetpoint.java
@@ -1,9 +1,8 @@
 package frc.lib.io.motor.setpoints;
 
-import edu.wpi.first.units.AngleUnit;
 import edu.wpi.first.units.measure.Angle;
 
-public class ProfiledPositionSetpoint extends BaseSetpoint<AngleUnit, Angle> {
+public class ProfiledPositionSetpoint extends PositionSetpoint {
     public ProfiledPositionSetpoint(Angle position) {
         super(position);
     }
