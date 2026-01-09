d4ecdcb640f911a669fd209b4144750f8d8ec515
Vimal Buckley <vimalbuckley@gmail.com>
2026-01-09 13:09:23 -0500

    Setpoint docs

--- src/main/java/frc/lib/io/motor/setpoints/IdleSetpoint.java
+++ src/main/java/frc/lib/io/motor/setpoints/IdleSetpoint.java
@@ -4,6 +4,9 @@ import edu.wpi.first.units.Units;
 import edu.wpi.first.units.measure.Dimensionless;
 
 public class IdleSetpoint extends BaseSetpoint<Dimensionless> {
+    /**
+     * Initializes an idle setpoint
+     */
     public IdleSetpoint() {
         super(Units.Value.of(0));
     }
